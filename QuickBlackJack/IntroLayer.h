d0a0580676e572867b484d61ad7c27cd01d6d7c1
Kayden Bui <vinhtruo@buffalo.edu>
2013-03-02 16:54:34 -0500

    Initial Commit

---
+++ QuickBlackJack/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  QuickBlackJack
+//
+//  Created by Kayden Bui on 3/2/13.
+//  Copyright Kayden Bui 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
